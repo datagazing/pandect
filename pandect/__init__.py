1ff3ec30bb4601d05a129c37a0185b8a8a9a712e
Brendan Strejcek <brendan@datagazing.com>
2021-06-17 14:13:34 -0400

    Bump version: 0.1.0 → 0.2.0

--- pandect/__init__.py
+++ pandect/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .pandect import Pandect, load, save, pandect, sav2dta
